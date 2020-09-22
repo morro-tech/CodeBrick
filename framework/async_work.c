11828db7c6ab1b95f9a30659b91a2447b3fc6b37
魔罗技术 <master_roger@sina.com>
2020-09-22 21:49:25 +0800

    增加异步作业管理

---
+++ framework/async_work.c
@@ -0,0 +1,100 @@
+/******************************************************************************
+ * @brief        异步作业
+ *
+ * Copyright (c) 2020, <master_roger@sina.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs: 
+ * Date           Author       Notes 
+ * 2020-09-22     Morro        Initial version. 
+ ******************************************************************************/
+#include "async_work.h"
+#include "comdef.h"
+#include <stdbool.h>
+
+/*******************************************************************************
+ * @brief       作业入队
+ * @param[in]   qlink     - 工作链
+ * @param[in]   new_item -  工作项
+ * @return      none
+ ******************************************************************************/
+static inline void workqueue_put(struct qlink *q, work_node_t *n)
+{
+	qlink_put(q, &n->node);                                  /*加入到就绪链表*/
+}
+
+
+/*******************************************************************************
+ * @brief       作业出队
+ * @param[in]   qlink     - 工作链
+ * @return      工作项
+ ******************************************************************************/
+static inline work_node_t *workqueue_get(struct qlink *q)
+{
+	struct qlink_node *n = qlink_get(q);
+	return n ? container_of(n, work_node_t, node) : NULL;
+}
+
+/*******************************************************************************
+ * @brief       作业预出队
+ * @param[in]   qlink     - 工作链
+ * @return      工作项
+ ******************************************************************************/
+static inline work_node_t * workqueue_peek(struct qlink *q)
+{
+	struct qlink_node *n = qlink_peek(q);
+	return n ? container_of(n, work_node_t, node) : NULL;
+}
+
+
+/*
+ * @brief       异常作业初始化
+ * @param[in]   w        - 作业管理器
+ * @param[in]   node_tbl - 作业节点表
+ * @param[in]   count    - node_tbl个数
+ */
+void async_work_init(async_work_t *w, work_node_t *node_tbl, int count)
+{
+    qlink_init(&w->idle);
+    qlink_init(&w->ready);
+    while (count--) {
+        qlink_put(&w->idle, &node_tbl->node);
+        node_tbl++;
+    }
+}
+
+/*
+ * @brief       增加作业到队列
+ * @param[in]   w        - 作业管理器
+ * @param[in]   params   - 作业参数
+ * @param[in]   work     - 作业入口
+ */
+bool async_work_add(async_work_t *w, void *object, void *params, 
+                    async_work_func_t work)
+{
+    work_node_t *n = workqueue_get(&w->idle);
+    if (n == NULL)
+        return false;
+    n->object = object;
+    n->params = params;
+    n->work   = work;
+    
+    workqueue_put(&w->ready, n);               /*加入到就绪链表*/          
+    return true;
+}
+
+
+/*
+ * @brief       异步作业处理
+ * @param[in]   w         - 作业管理器
+ */
+void async_work_process(async_work_t *w)
+{
+    work_node_t *n;
+    if ((n = workqueue_get(&w->ready)) == NULL)
+        return;
+    n->work(w, n->object, n->params);
+    workqueue_put(&w->idle, n);
+}
+
---
+++ framework/async_work.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * @brief        异步作业
+ *
+ * Copyright (c) 2020, <master_roger@sina.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs: 
+ * Date           Author       Notes 
+ * 2020-09-22     Morro        Initial version. 
+ ******************************************************************************/
+#ifndef _ASYNC_WORK_H_
+#define _ASYNC_WORK_H_
+
+#include "qlink.h"
+#include <stdbool.h>
+
+/*异步作业管理器 -------------------------------------------------------------*/
+typedef struct {
+    struct qlink idle;                             /*空闲队列*/
+    struct qlink ready;                            /*就绪队列*/
+}async_work_t;
+
+typedef void (*async_work_func_t)(async_work_t *w, void *object, void *params);
+
+/*异步作业结点 ---------------------------------------------------------------*/
+typedef struct {
+    void *object;                                  /*作业对象*/
+    void *params;                                  /*作业参数*/    
+    async_work_func_t work;                        /*作业入口*/
+    struct qlink_node node;                        /*链表节点*/
+}work_node_t;
+
+void async_work_init(async_work_t *w, work_node_t *nodes, int max_nodes);
+
+bool async_work_add(async_work_t *w, void *object, void *params, 
+                    async_work_func_t work);
+
+void async_work_process(async_work_t *w);
+
+#endif
